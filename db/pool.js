fb62bb45ed419a853ab835bb6e73ce40b44b6efe
Talha2195 <143935671+Talha2195@users.noreply.github.com>
2024-11-12 12:45:56 +0300

    Slight changes to launch database

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,12 @@
-const { Pool } = require('pg');
 require('dotenv').config();
+const { Pool } = require('pg');
+
+console.log('DATABASE_URL:', process.env.DATABASE_URL); 
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, 
+    rejectUnauthorized: false,  
   },
 });
 
