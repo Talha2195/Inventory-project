b3c56ba295d1e84df6f7939501a13fe7974f442b
Talha2195 <143935671+Talha2195@users.noreply.github.com>
2024-11-12 13:01:54 +0300

    Database updated

--- db/populateDb.js
+++ db/populateDb.js
@@ -5,7 +5,9 @@ const connectionString = process.env.DATABASE_URL;
 
 const client = new Client({
   connectionString: connectionString,
-  ssl: false,
+  ssl: {
+    rejectUnauthorized: false,  
+  },
 });
 
 async function seedDatabase() {
